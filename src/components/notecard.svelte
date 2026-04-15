1140702b957cfd6426e65950519f5a8f75c3b759
Kimchu16 <kimberlylibranza@gmail.com>
2026-04-15 01:23:38 +0100

    feat: update notecard component to display anchored status instead of position values

--- src/components/notecard.svelte
+++ src/components/notecard.svelte
@@ -9,9 +9,11 @@ import { modalData } from '$lib/stores';
     <h2 class="card-title">Note</h2>
     <p class="text-base-content/80">{note.context}</p>
     <div class="card-actions mt-2">
-      <div class="badge badge-outline">x: {Number(note.pos_x).toFixed(3)}</div>
-      <div class="badge badge-outline">y: {Number(note.pos_y).toFixed(3)}</div>
-      <div class="badge badge-outline">z: {Number(note.pos_z).toFixed(3)}</div>
+      {#if note.is_anchored}
+        <div class="badge badge-primary badge-outline">Anchored</div>
+      {:else}
+        <div class="badge badge-outline">Unanchored</div>
+      {/if}
     </div>
     <button onclick={() => {modalData.set(note)}} class="btn btn-warning w-32 ml-auto">Edit</button>
   </div>
